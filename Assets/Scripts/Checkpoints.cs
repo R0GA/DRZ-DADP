1d48c97b59f00794e16d047664b4b6def22200b1
R0GA <rowynn.davies@gmail.com>
2024-08-16 13:48:25 +0200

    Added all checkpoints + added particle effect for checkpoints

--- Assets/Scripts/Checkpoints.cs
+++ Assets/Scripts/Checkpoints.cs
@@ -5,11 +5,19 @@ using UnityEngine.Rendering.Universal;
 
 public class Checkpoints : MonoBehaviour
 {
+    private ParticleSystem checkpointParticles;
+
+    private void Awake()
+    {
+        checkpointParticles = GetComponentInChildren<ParticleSystem>();
+    }
+
     private void OnTriggerEnter(Collider other)
     {
         if (other.CompareTag("Player"))
         {
             other.gameObject.GetComponent<FirstPersonControls>().currentCheckpoint = transform.position;
+            checkpointParticles.Stop();
             //Debug.Log(other.gameObject.GetComponent<FirstPersonControls>().currentCheckpoint);
         }
     }
